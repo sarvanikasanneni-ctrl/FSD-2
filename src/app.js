373803a61ee26afe7d5a4b2d4f2c6165224c6916
Sarvani <sarvanikasanneni@gmail.com>
2026-07-13 10:51:26 +0530

    My first TypeScript program

---
+++ src/app.js
@@ -0,0 +1,7 @@
+"use strict";
+let studentName = "Sarvani";
+let age = 18;
+let isStudent = true;
+console.log(studentName);
+console.log(age);
+console.log(isStudent);
---
+++ src/app.ts
@@ -0,0 +1,7 @@
+let studentName: string = "Sarvani";
+let age: number = 18;
+let isStudent: boolean = true;
+
+console.log(studentName);
+console.log(age);
+console.log(isStudent);
